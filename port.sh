02b36f3e4a24d903978c563a8500f28d63e6b116
Cloud User <ec2-user@ip-172-31-40-9.ap-south-1.compute.internal>
2023-05-06 07:05:16 +0000

    adding tc

---
+++ port.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#set -x
+port=$(sudo cat server.xml | grep "Connector port"  | grep "HTTP" | grep -o 'port="[0-9]*"' | sed 's/port="\([0-9]*\)"/\1/')
+
+#
+#curl ifconfig.me
+echo -e " log into this address for tomcat $(curl -s ifconfig.me):$port "
---
+++ tc_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+sudo yum update -y 1>/dev/null
+sudo yum install -y java-11-openjdk wget 1>/dev/null
+#
+wget –c https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.74/bin/apache-tomcat-9.0.74.tar.gz
+#
+sudo mkdir -p /opt/tomcat
+sudo tar xf apache-tomcat-9.0.74.tar.gz -C /opt/tomcat
+#
+#
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.74/webapps/manager/META-INF/context.xml
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.74/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.74/conf/tomcat-users.xml
+sudo cp server.xml /opt/tomcat/apache-tomcat-9.0.74/conf/server.xml
+#
+sudo useradd -r -M -U -d /opt/tomcat -s /bin/false tomcat 2>/dev/null
+sudo chown -R tomcat: /opt/tomcat/*
+sudo cp tomcat.service /etc/systemd/system/tomcat.service
+#
+sudo systemctl daemon-reload
+#
+sudo systemctl start tomcat
+#
+port=$(sudo cat server.xml | grep "Connector port"  | grep "HTTP" | grep -o 'port="[0-9]*"' | sed 's/port="\([0-9]*\)"/\1/')
+#
+#
+echo -e " log into this public ip $(curl -s ifconfig.me):$port "
